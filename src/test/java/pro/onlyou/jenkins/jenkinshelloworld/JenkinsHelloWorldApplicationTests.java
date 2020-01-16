e45512a741996e42dfda3b118f207265946d43a3
jiangchangxiong <1327994105@qq.com>
2020-01-16 14:17:13 +0800

    初始化springboot项目

---
+++ src/test/java/pro/onlyou/jenkins/jenkinshelloworld/JenkinsHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.onlyou.jenkins.jenkinshelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
